1e19b5e4df60d4547f6754170d110d54e13815c1
duduhazan <duduhazan123@gmail.com>
2023-07-18 23:25:08 +0300

    adding authMiddleWare & cookieParser & userRouter

--- server/main.js
+++ server/main.js
@@ -3,7 +3,10 @@ import cors from "cors";
 import { tasksRouter } from "./routes/tasks.route";
 import { connectDB } from "./connect-to-db";
 import { SignInRouter } from "./routes/signin.route";
-import { SignUpRouter } from "./routes/signup";
+import { SignUpRouter } from "./routes/signup.route";
+import { authMiddleWare } from "./middlewares/auth.middleware";
+import cookieParser from "cookie-parser";
+import { UserRouter } from "./routes/user.route";
 
 async function startServer() {
   // await connectDB(process.env.DB_URL);
@@ -14,14 +17,21 @@ async function startServer() {
   await connectDB("mongodb://localhost:27017/to-do");
   const port = 3500;
   const app = express();
+  const secret = "secret!-)";
 
   app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
+  app.use(cookieParser());
+
   app.use(express.json());
 
-    app.use(SignInRouter());
+  app.use(SignInRouter(secret));
 
-    app.use(SignUpRouter());
+  app.use(SignUpRouter());
+
+  app.use(authMiddleWare(secret));
+  
+  app.use(UserRouter());
 
   app.use(tasksRouter());
 
